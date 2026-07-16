22c4b9716debd1840e319625cf55b69cb93f2add
ohmycode-cn <ohcode@163.com>
2026-07-16 23:24:31 +0800

    Adapt C initialization script for C language
    - Modify mkfc.sh to initialize C source and header files instead of C++.
    - Replace C++ specific constructs: remove namespaces and classes.
    - Use 'ruac_c_' prefix for all generated files (both .c and .h).
    - Update file templates with C-style header guards and extern "C" blocks.
    - Adjust directory paths to src/cc/include and src/cc/src.
    - Include .gitkeep files to preserve directory structure in version control.

--- mkfc.sh
+++ mkfc.sh
@@ -6,9 +6,9 @@
 # | Bash: Recommended using bash version 5.0 or higher
 # | Bash Style Guide: RUAC-BASH-STYLE-GUIDE.md
 # | Bash Style Inherited From: https://github.com/ohmycode-cn/codestyle/OMC-BASH-STYLE-GUIDE.md
-# | Description: Initialize the project c source and header files.
+# | Description: Initialize the project C source and header files.
 # | Usage:
-# |      1 - bash mkfc.sh
+# |      1 - bash mkfc.sh [path] [file_name]
 # *********************************************************************************************
 if [[ "${0}" != "${BASH_SOURCE[0]}" ]]; then
     echo -e "[EXECUTE ERROR]: Correct execute method is \"bash ${0}\""
@@ -34,3 +34,108 @@ if [[ ${ONCE_RET} -ne 0 ]]; then
     return 1 &>/dev/null
 fi
 unset ONCE_RET # delete global variable ONCE_RET
+
+function main() {
+    local root_path
+    root_path="$(pwd)"
+    local inc="${root_path}/src/cc/include"
+    local src="${root_path}/src/cc/src"
+
+    local file_name_prefix="ruac_c_"
+    local file="${file_name_prefix}${2}"
+    local path="${1}"
+
+    if [[ -z "${path}" ]]; then
+        msg_warning "No path parameter, use default path: src/cc{include/ and src/}"
+    else
+        msg_info "Use path: src/cc{include/${path} and src/cc/src/${path}}"
+        inc="${inc}/${path}"
+        src="${src}/${path}"
+
+    fi
+
+    if [[ -z "${file}" ]]; then
+        msg_error "No file parameter! You must provide a file name."
+        return 1
+    fi
+
+    if [[ ! -d "${inc}" || ! -d "${src}" ]]; then
+        msg_error "Include path 'src/cc/include/${path}' or src path 'src/cc/src/${path}' not exist!"
+        return 1
+    fi
+
+    if [[ -f "${src}/${file}.c" || -f "${inc}/${file}.h" ]]; then
+        msg_error "File ${file}.c or ${file}.h already exists, aborting."
+        return 1
+    fi
+
+    local n_time
+    n_time="$(date '+%Y-%m-%d %H:%M:%S')"
+
+    local whoami
+    whoami="$(cat .current_author.config | tr -d '\n\r\t')"
+
+    local publuc_init_content=(
+        "/**"
+        " * C Style Guide: RUAC-C-STYLE-GUIDE.md"
+        " * Line Limit: Recommend line breaks at col: 96, force line breaks at col: 120"
+        " * Date Time: ${n_time}"
+        " * Author: ${whoami}"
+        " * Header File : include/${path}/${file}.h"
+        " * Source File : src/${path}/${file}.c"
+        " * "
+        " * File Function Description:"
+        " * "
+    )
+
+    local upper_case_file_name="${file^^}_H"
+    local header_init_content=(
+        " * "
+        " */"
+        ""
+        "#pragma once"
+        "#ifndef ${upper_case_file_name}"
+        "#define ${upper_case_file_name}"
+        ""
+        "#ifdef __cplusplus"
+        "extern \"C\" {"
+        "#endif"
+        ""
+        "/*"
+        " * @brief Placeholder function declaration."
+        " */"
+        "void placeholder_function(void);"
+        ""
+        "#ifdef __cplusplus"
+        "}"
+        "#endif"
+        ""
+        "#endif /* ${upper_case_file_name} */"
+    )
+
+    local source_init_content=(
+        " * "
+        " */"
+        ""
+        "#include \"${path}/${file}.h\""
+        ""
+        "/*"
+        " * @brief Placeholder function definition."
+        " */"
+        "void placeholder_function(void) {"
+        "    /* TODO: implement */"
+        "}"
+    )
+
+    # init header file
+    printf '%s\n' "${publuc_init_content[@]}" >"${inc}/${file}.h"
+    printf '%s\n' "${header_init_content[@]}" >>"${inc}/${file}.h"
+
+    # init source file
+    printf '%s\n' "${publuc_init_content[@]}" >"${src}/${file}.c"
+    printf '%s\n' "${source_init_content[@]}" >>"${src}/${file}.c"
+
+    msg_info "Init done! (c and h){file: ${file}}"
+}
+main "$@"
+exit 0
