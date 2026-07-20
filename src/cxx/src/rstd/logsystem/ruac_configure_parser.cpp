7703fa13fb5c52fe55f9d76dfcbb895a7860567a
ohmycode-cn <ohcode@163.com>
2026-07-20 18:06:55 +0800

    feat(logsystem): populate full default configuration map in ConfigureParser
    - Add all 13 configuration entries from ruacdb.log.conf as defaults
    - Use logkeys::words constants for values instead of raw string literals
    - Covers terminal modes, log level filters, format modes, output mode,
      write path, file name, and file size limit

--- src/cxx/src/rstd/logsystem/ruac_configure_parser.cpp
+++ src/cxx/src/rstd/logsystem/ruac_configure_parser.cpp
@@ -13,13 +13,28 @@
 
 #include "rstd/logsystem/ruac_configure_parser.hpp"
 #include "rstd/logsystem/ruac_loadconf.hpp"
+#include "rstd/logsystem/ruac_logkeys.hpp"
 
 namespace ruac::rstd::logsystem {
 
     namespace {
 
         auto retDefaultConfigureMap() -> logtype::smap {
-            return {};
+            return {
+                {logkeys::words::G_ENABLE_TERM_COMPATIBLE_MODE, logkeys::words::G_TRUE},
+                {logkeys::words::G_ENABLE_TERM_HIGHLIGHT_MODE, logkeys::words::G_FALSE},
+                {logkeys::words::G_ENABLE_TERM_BOLD_FONT_MODE, logkeys::words::G_FALSE},
+                {logkeys::words::G_ENABLE_TERM_LOG_PRINT_MODE, logkeys::words::G_FALSE},
+                {logkeys::words::G_LOG_TERM_LEVEL_FILTER, logkeys::words::G_LOG_LEVEL_DEBUG},
+                {logkeys::words::G_LOG_FILE_LEVEL_FILTER, logkeys::words::G_LOG_LEVEL_DEBUG},
+                {logkeys::words::G_LOG_MIN_LEVEL_FILTER, logkeys::words::G_LOG_LEVEL_DEBUG},
+                {logkeys::words::G_LOG_TERM_FORMAT_MODE, logkeys::words::G_TEXT},
+                {logkeys::words::G_LOG_FILE_FORMAT_MODE, logkeys::words::G_TEXT},
+                {logkeys::words::G_LOG_OUTPUT_MODE, logkeys::words::G_CONSOLE},
+                {logkeys::words::G_LOG_WRITE_PATH, "/home/repox/Engineering/ruacdbv01/ruacdb/tmp"},
+                {logkeys::words::G_LOG_WRITE_FILE, "ruacdb.test.log"},
+                {logkeys::words::G_LOG_FILE_SIZE_LIMIT, "128MB"},
+            };
         }
 
     } // namespace
